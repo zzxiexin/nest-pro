599acab73fb7e0849205a311536c5fabaa13eb75
zzxiexin <344391941@qq.com>
2024-05-05 14:09:16 +0800

    feat: stage

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggerMiddleware } from './logger/logger.middleware';
+import { HttpExceptionFilter } from './error/error';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // app.use(LoggerMiddleware);
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
