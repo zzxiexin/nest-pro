d140923cdb412d7a83c24384d68ebf6acce97e48
zzxiexin <344391941@qq.com>
2024-05-06 20:34:41 +0800

    feat: 添加全局中间件

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { LoggerMiddleware } from './logger/logger.middleware';
+import { logger } from './logger/logger.middleware';
 import { HttpExceptionFilter } from './error/error';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.use(LoggerMiddleware);
+  app.use(logger);
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
 }
